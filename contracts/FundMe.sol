80541253f27e4340cf79915a7c773718de9aedb1
flydevox <flydevoxxx@gmail.com>
2025-03-13 21:35:16 +0100

    fundMe part3

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -1,24 +1,49 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.18;
 
-import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 contract FundMe {
+    using PriceConverter for uint256;
 
-    uint256 public minimumUsd  = 5;
+    uint256 public minimumUsd  = 5e18 ;
+
+    address[] public funders;
+    mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
 
     function fund() public payable {
-        require(msg.value > minimumUsd, "didn't send enough Ethereum");
+        require(msg.value.getConversionRate() >= minimumUsd, "didn't send enough Ethereum");
+        funders.push(msg.sender);
+        addressToAmountFunded[msg.sender] += msg.value;
     }
 
-    function getPrice() public {
-        //0x694AA1769357215DE4FAC081bf1f309aDC325306
+    function withdraw() public  onlyOwner {
+        
+        for(uint256 funderIndex = 0; funderIndex < funders.length; funderIndex++) {
+            address funder = funders[funderIndex];
+            addressToAmountFunded[funder] = 0;
+        }
+        funders = new address[](0);
+        // // transfer
+        // payable(msg.sender).transfer(address(this).balance);
 
+        // // send
+        // bool sendSuccess = payable(msg.sender).send(address(this).balance);
+        // require(sendSuccess, "Send failed");
+
+        // call
+        (bool callSuccess,) = payable(msg.sender).call{value: address(this).balance}("");
+        require(callSuccess, "Call failed");
     }
-    function getConversionRate() public {}
 
-    function getVersion() public view returns (uint256) {
-        AggregatorV3Interface priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
-        return priceFeed.version();
-    } 
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Sender is not owner!");
+        _;
+    }
 }
